a89a184ce9b048d6b3f6f79206aecee2f5991d99
Akshit <akshitvirkud@gmail.com>
2026-04-13 14:11:12 +0530

    UC2: Display Hello with Command-Line Argument

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,14 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        // Check if argument is given
+        if (args.length > 0) {
+            String name = args[0];   // take first argument
+            System.out.println("Hello, " + name + "!");
+        } else {
+            // default output if no argument
+            System.out.println("Hello, World!");
+        }
     }
 }
