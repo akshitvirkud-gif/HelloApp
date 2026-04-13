dacb94baf1e41faaffaf1e5b830bc4da938b6194
Akshit <akshitvirkud@gmail.com>
2026-04-14 01:36:05 +0530

    UC3: Display Hello with command-line argument or default message

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,13 +3,12 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-        // Check if argument is given
+        String name = "World";
+
         if (args.length > 0) {
-            String name = args[0];   // take first argument
-            System.out.println("Hello, " + name + "!");
-        } else {
-            // default output if no argument
-            System.out.println("Hello, World!");
+            name = args[0];
         }
+
+        System.out.println("Hello, " + name + "!");
     }
 }
